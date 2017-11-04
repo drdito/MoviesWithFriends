e942687aa477a136d5979f5a840cdb9dba8d4eec
Unknown <drdito8@gmail.com>
2017-11-04 10:37:08 -0700

    server.js work, added comments and more middleware to use passport-local

--- server.js
+++ server.js
@@ -1,29 +1,51 @@
-var express = require('express');
-var methodOverride = require('method-override');
-var bodyParser = require('body-parser');
+//*************** Robot Unicorn Attack*********************
+//***************Movies with Friends***********************
+//***************server.js*********************************
 
-var port = process.env.PORT || 8080;
+//Bringing in necessary dependencies.
+var express = require('express'),
+passport = require('passport'),
+port = process.env.PORT || 8080;
+bodyParser = require('body-parser'),
+cookieParser = require('cookie-parser'),
+session = require('express-session'),
+exphbs = require("express-handlebars");
+User = require('./models/user'),
+app = express();
 
-var app = express();
+//Middleware declaration for body-parser
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+//Allows us to use local styling from our public/assets folder.
+app.use(express.static(__dirname + "./public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(methodOverride('X-HTTP-Method-Override'));
-
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+//Handlebars Engine
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/friends_controller.js");
+//Define controller to bring back our user data from user model
+var userRoutes = require("./controllers/userController.js");
+app.use("/login", routes);
 
-app.use("/", routes);
+//Middleware to authenticate through passport
+app.use(bodyParser());
+app.use(require('connect-multiparty')());
+app.use(cookieParser());
+app.use(session({ secret: 'super-secret' }));
 
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Syncing with the database prior to listening on port 8080
+User.sequelize.sync().then(function() {
+  app.listen(port);
 });
