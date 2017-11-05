d7414cfcf0ad95a294c1b808b3a2be595c12f617
Your NameNLegge <little.legge@gmail.com>
2017-11-04 17:41:57 -0700

    Created model and controller for reddit data. Removed html manipulation from on click and added some basic stuff to index.handlebars.

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -17,25 +17,7 @@ function redditCall() {
       raw_json: 1
     },
     success: function (response) {
-      var children = response.data.children;
-      $.each(children, function (i, post) {
-        if (post.data.selftext_html) {
-          $("#content").append("<div><h3>" +
-            post.data.title + "</h3><p>Author: " +
-            post.data.author + "</p><p>Reddit Score: " +
-            post.data.score + "</p><p>" +
-            post.data.selftext_html + "</p><p><a href='" +
-            post.data.url + "'>Source</a></p></div>");
-        }
-        else {
-          $("#content").append("<div><h3>" +
-            post.data.title + "</h3><p>Author: " +
-            post.data.author + "</p><p>Reddit Score: " +
-            post.data.score + "</p><img src='" +
-            post.data.thumbnail + "' alt='preview image'><p><a href='" +
-            post.data.url + "'>Source</a></p></div>");
-        }
-      })
+      console.log(response);
     }
   });
 };
