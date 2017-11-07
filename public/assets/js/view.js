7354bb59edbaa90a06753eccb15c27ec97bddc51
Your NameNLegge <little.legge@gmail.com>
2017-11-06 20:00:46 -0700

    Ajax call to OMDB API for posters

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -1,10 +1,16 @@
 // When the url changes, run the function
-window.onhashchange = pleaseWork();
+window.onhashchange = ProfileUpload();
 // If the profile page is loaded, run an ajax call to the reddit API
-function pleaseWork() {
+function ProfileUpload() {
+  console.log("The username is " + $("#user").html());
   if (location.href === "http://localhost:8080/profile") {
     // console.log("user is " + $("#user").html());
     redditCall();
+    OMDBCall0();
+    OMDBCall1();
+    OMDBCall2();
+    OMDBCall3();
+    OMDBCall4();
   }
 }
 
@@ -59,3 +65,84 @@ $(document).ready(function () {
   });
 
 });
+
+// AJAX call to grab movie posters from OMDB API 
+function OMDBCall0() {
+  var movieName = ($("#title0").html());
+  $.ajax({
+    type: "GET",
+    url: "http://www.omdbapi.com/",
+    data: {
+      apikey: "d577c18b",
+      t: movieName,
+      type: "movie"
+    },
+    success: function (response) {
+      $("#movie0").attr("src", response.Poster);
+    }
+  });
+};
+
+function OMDBCall1() {
+  var movieName = ($("#title1").html());
+  $.ajax({
+    type: "GET",
+    url: "http://www.omdbapi.com/",
+    data: {
+      apikey: "d577c18b",
+      t: movieName,
+      type: "movie"
+    },
+    success: function (response) {
+      $("#movie1").attr("src", response.Poster);
+    }
+  });
+};
+
+function OMDBCall2() {
+  var movieName = ($("#title2").html());
+  $.ajax({
+    type: "GET",
+    url: "http://www.omdbapi.com/",
+    data: {
+      apikey: "d577c18b",
+      t: movieName,
+      type: "movie"
+    },
+    success: function (response) {
+      $("#movie2").attr("src", response.Poster);
+    }
+  });
+};
+
+function OMDBCall3() {
+  var movieName = ($("#title3").html());
+  $.ajax({
+    type: "GET",
+    url: "http://www.omdbapi.com/",
+    data: {
+      apikey: "d577c18b",
+      t: movieName,
+      type: "movie"
+    },
+    success: function (response) {
+      $("#movie3").attr("src", response.Poster);
+    }
+  });
+};
+
+function OMDBCall4() {
+  var movieName = ($("#title4").html());
+  $.ajax({
+    type: "GET",
+    url: "http://www.omdbapi.com/",
+    data: {
+      apikey: "d577c18b",
+      t: movieName,
+      type: "movie"
+    },
+    success: function (response) {
+      $("#movie4").attr("src", response.Poster);
+    }
+  });
+};
