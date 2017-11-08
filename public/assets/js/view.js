afeb7a33008ba3022884604682bb7925629159d6
Your NameNLegge <little.legge@gmail.com>
2017-11-07 20:10:34 -0700

    Playing with friends formatting

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -1,10 +1,10 @@
 // When the url changes, run the function
 window.onhashchange = ProfileUpload();
-// If the profile page is loaded, run an ajax call to the reddit API
+// If the profile page is loaded, render data from reddit and omdb
 function ProfileUpload() {
   console.log("The username is " + $("#user").html());
   if (location.href === "http://localhost:8080/profile") {
-    // console.log("user is " + $("#user").html());
+    // This is ugly, but for a reason. See notes below...
     redditCall0();
     redditCall1();
     redditCall2();
@@ -18,197 +18,121 @@ function ProfileUpload() {
   }
 }
 
-// Ajax call to return reddit info and write to HTML     
+// Ajax call to return reddit info and write to HTML. 
+// I had to do these in separate functions to prevent the browser from locking up.    
 function redditCall0() {
-
   var movieName = ($("#title0").html());
-  $.ajax({
-    type: "GET",
-    url: "https://www.reddit.com/r/movies/search.json",
-    data: {
-      q: movieName,
-      restrict_sr: true,
-      sort: "top",
-      t: "month",
-      limit: 5,
-      raw_json: 1
-    },
-    success: function (response) {
-      var children = response.data.children;
-      $.each(children, function (i, post) {
-        if (post.data.selftext_html) {
-          $("#redditData").append("<li class='redditPost'><h6><a href='" +
-            post.data.url + "' target='_blank'>" +
-            post.data.title + "</h6></a><p>Author: " +
-            post.data.author + "</p><p>Reddit Score: " +
-            post.data.score + "</p><p>" +
-            post.data.selftext_html + "</p></li><hr>");
-        }
-        else {
-          $("#redditData").append("<li class='redditPost'><h6><a href='" +
-            post.data.url + "' target='_blank'>" +
-            post.data.title + "</h6></a><p>Author: " +
-            post.data.author + "</p><p>Reddit Score: " +
-            post.data.score + "</p><img src='" +
-            post.data.thumbnail + "' alt='preview image'></li><hr>");
-        }
-      })
-    }
-  });
+  redditAjax(movieName);
 };
 
 function redditCall1() {
-
   var movieName = ($("#title1").html());
-  $.ajax({
-    type: "GET",
-    url: "https://www.reddit.com/r/movies/search.json",
-    data: {
-      q: movieName,
-      restrict_sr: true,
-      sort: "top",
-      t: "month",
-      limit: 5,
-      raw_json: 1
-    },
-    success: function (response) {
-      var children = response.data.children;
-      $.each(children, function (i, post) {
-        if (post.data.selftext_html) {
-          $("#redditData").append("<li class='redditPost'><h6><a href='" +
-            post.data.url + "' target='_blank'>" +
-            post.data.title + "</h6></a><p>Author: " +
-            post.data.author + "</p><p>Reddit Score: " +
-            post.data.score + "</p><p>" +
-            post.data.selftext_html + "</p></li><hr>");
-        }
-        else {
-          $("#redditData").append("<li class='redditPost'><h6><a href='" +
-            post.data.url + "' target='_blank'>" +
-            post.data.title + "</h6></a><p>Author: " +
-            post.data.author + "</p><p>Reddit Score: " +
-            post.data.score + "</p><img src='" +
-            post.data.thumbnail + "' alt='preview image'></li><hr>");
-        }
-      })
-    }
-  });
+  redditAjax(movieName);
 };
 
 function redditCall2() {
-
   var movieName = ($("#title2").html());
-  $.ajax({
-    type: "GET",
-    url: "https://www.reddit.com/r/movies/search.json",
-    data: {
-      q: movieName,
-      restrict_sr: true,
-      sort: "top",
-      t: "month",
-      limit: 5,
-      raw_json: 1
-    },
-    success: function (response) {
-      var children = response.data.children;
-      $.each(children, function (i, post) {
-        if (post.data.selftext_html) {
-          $("#redditData").append("<li class='redditPost'><h6><a href='" +
-            post.data.url + "' target='_blank'>" +
-            post.data.title + "</h6></a><p>Author: " +
-            post.data.author + "</p><p>Reddit Score: " +
-            post.data.score + "</p><p>" +
-            post.data.selftext_html + "</p></li><hr>");
-        }
-        else {
-          $("#redditData").append("<li class='redditPost'><h6><a href='" +
-            post.data.url + "' target='_blank'>" +
-            post.data.title + "</h6></a><p>Author: " +
-            post.data.author + "</p><p>Reddit Score: " +
-            post.data.score + "</p><img src='" +
-            post.data.thumbnail + "' alt='preview image'></li><hr>");
-        }
-      })
-    }
-  });
+  redditAjax(movieName);
 };
 
 function redditCall3() {
-
   var movieName = ($("#title3").html());
-  $.ajax({
-    type: "GET",
-    url: "https://www.reddit.com/r/movies/search.json",
-    data: {
-      q: movieName,
-      restrict_sr: true,
-      sort: "top",
-      t: "month",
-      limit: 5,
-      raw_json: 1
-    },
-    success: function (response) {
-      var children = response.data.children;
-      $.each(children, function (i, post) {
-        if (post.data.selftext_html) {
-          $("#redditData").append("<li class='redditPost'><h6><a href='" +
-            post.data.url + "' target='_blank'>" +
-            post.data.title + "</h6></a><p>Author: " +
-            post.data.author + "</p><p>Reddit Score: " +
-            post.data.score + "</p><p>" +
-            post.data.selftext_html + "</p></li><hr>");
-        }
-        else {
-          $("#redditData").append("<li class='redditPost'><h6><a href='" +
-            post.data.url + "' target='_blank'>" +
-            post.data.title + "</h6></a><p>Author: " +
-            post.data.author + "</p><p>Reddit Score: " +
-            post.data.score + "</p><img src='" +
-            post.data.thumbnail + "' alt='preview image'></li><hr>");
-        }
-      })
-    }
-  });
+  redditAjax(movieName);
 };
 
 function redditCall4() {
-
   var movieName = ($("#title4").html());
+  redditAjax(movieName);
+};
+
+// AJAX call to grab movie posters from OMDB API 
+// I had to do these in separate functions to prevent the browser from locking up.  
+function OMDBCall0() {
+  var movieName = ($("#title0").html());
+  var x = 0;
+  omdbAjax(movieName, x);
+};
+
+function OMDBCall1() {
+  var movieName = ($("#title1").html());
+  var x = 1;
+  omdbAjax(movieName, x);
+};
+
+function OMDBCall2() {
+  var movieName = ($("#title2").html());
+  var x = 2;
+  omdbAjax(movieName, x);
+};
+
+function OMDBCall3() {
+  var movieName = ($("#title3").html());
+  var x = 3;
+  omdbAjax(movieName, x);
+};
+
+function OMDBCall4() {
+  var movieName = ($("#title4").html());
+  var x = 4;
+  omdbAjax(movieName, x);
+};
+
+function redditAjax(movieName) {
   $.ajax({
     type: "GET",
     url: "https://www.reddit.com/r/movies/search.json",
     data: {
       q: movieName,
       restrict_sr: true,
-      sort: "top",
+      sort: "relevance",
       t: "month",
       limit: 5,
       raw_json: 1
     },
     success: function (response) {
-      var children = response.data.children;
-      $.each(children, function (i, post) {
-        if (post.data.selftext_html) {
-          $("#redditData").append("<li class='redditPost'><h6><a href='" + 
-            post.data.url + "' target='_blank'>" +
-            post.data.title + "</h6></a><p>Author: " +
-            post.data.author + "</p><p>Reddit Score: " +
-            post.data.score + "</p><p>" +
-            post.data.selftext_html + "</p></li><hr>");
-        }
-        else {
-          $("#redditData").append("<li class='redditPost'><h6><a href='" +
-            post.data.url + "' target='_blank'>" +
-            post.data.title + "</h6></a><p>Author: " +
-            post.data.author + "</p><p>Reddit Score: " +
-            post.data.score + "</p><img src='" +
-            post.data.thumbnail + "' alt='preview image'></li><hr>");
-        }
-      })
+      postReddit(response);
     }
   });
 };
 
+function postReddit(response) {
+  var children = response.data.children;
+  $.each(children, function (i, post) {
+    if (post.data.selftext_html) {
+      $("#redditData").append("<li class='redditPost'><h6><a href='" +
+        post.data.url + "' target='_blank'>" +
+        post.data.title + "</h6></a><p>Author: " +
+        post.data.author + "</p><p>Reddit Score: " +
+        post.data.score + "</p><p>" +
+        post.data.selftext_html + "</p></li><hr>");
+    }
+    else {
+      $("#redditData").append("<li class='redditPost'><h6><a href='" +
+        post.data.url + "' target='_blank'>" +
+        post.data.title + "</h6></a><p>Author: " +
+        post.data.author + "</p><p>Reddit Score: " +
+        post.data.score + "</p><img src='" +
+        post.data.thumbnail + "' alt='preview image'></li><hr>");
+    }
+  });
+};
+
+function omdbAjax(movieName, x) {
+  $.ajax({
+    type: "GET",
+    url: "http://www.omdbapi.com/",
+    data: {
+      apikey: "d577c18b",
+      t: movieName,
+      type: "movie"
+    },
+    success: function (response) {
+      $("#movie" + x).attr("src", response.Poster);
+    }
+  });
+};
+
+// Render scrollbar on ready
 $(document).ready(function () {
 
   $('#sidebar').niceScroll({
@@ -222,84 +146,3 @@ $(document).ready(function () {
   });
 
 });
-
-// AJAX call to grab movie posters from OMDB API 
-function OMDBCall0() {
-  var movieName = ($("#title0").html());
-  $.ajax({
-    type: "GET",
-    url: "http://www.omdbapi.com/",
-    data: {
-      apikey: "d577c18b",
-      t: movieName,
-      type: "movie"
-    },
-    success: function (response) {
-      $("#movie0").attr("src", response.Poster);
-    }
-  });
-};
-
-function OMDBCall1() {
-  var movieName = ($("#title1").html());
-  $.ajax({
-    type: "GET",
-    url: "http://www.omdbapi.com/",
-    data: {
-      apikey: "d577c18b",
-      t: movieName,
-      type: "movie"
-    },
-    success: function (response) {
-      $("#movie1").attr("src", response.Poster);
-    }
-  });
-};
-
-function OMDBCall2() {
-  var movieName = ($("#title2").html());
-  $.ajax({
-    type: "GET",
-    url: "http://www.omdbapi.com/",
-    data: {
-      apikey: "d577c18b",
-      t: movieName,
-      type: "movie"
-    },
-    success: function (response) {
-      $("#movie2").attr("src", response.Poster);
-    }
-  });
-};
-
-function OMDBCall3() {
-  var movieName = ($("#title3").html());
-  $.ajax({
-    type: "GET",
-    url: "http://www.omdbapi.com/",
-    data: {
-      apikey: "d577c18b",
-      t: movieName,
-      type: "movie"
-    },
-    success: function (response) {
-      $("#movie3").attr("src", response.Poster);
-    }
-  });
-};
-
-function OMDBCall4() {
-  var movieName = ($("#title4").html());
-  $.ajax({
-    type: "GET",
-    url: "http://www.omdbapi.com/",
-    data: {
-      apikey: "d577c18b",
-      t: movieName,
-      type: "movie"
-    },
-    success: function (response) {
-      $("#movie4").attr("src", response.Poster);
-    }
-  });
-};
